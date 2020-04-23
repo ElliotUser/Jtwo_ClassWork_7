0eeb0e95ed5fe258c64fc15bc80fe95b45e2fec6
MRParsifal <aleksandr-gavrilyuk@mail.ru>
2020-04-23 18:50:27 +0300

    ClassWork 7(Chat)

---
+++ network/src/ru/rickSanchez/jtwo/network/SocketThread.java
@@ -0,0 +1,67 @@
+package ru.rickSanchez.jtwo.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketThread extends Thread{
+
+    private Socket socket;
+    private SocketThreadListener listener;
+    private DataOutputStream out;
+
+    public SocketThread(SocketThreadListener listener, String name, Socket socket){
+        super();
+        this.socket = socket;
+        this.listener = listener;
+        start();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            DataInputStream in = new DataInputStream(socket.getInputStream()) ;
+            out = new DataOutputStream(socket.getOutputStream());
+            listener.onSocketStart(this, socket);
+            listener.onSocketReady(this, socket);
+            while(!isInterrupted()){
+                String msg = in.readUTF();
+                listener.onReceiveString(this, socket, msg);
+            }
+        } catch(IOException e) {
+            listener.onSocketException(this, e);
+        } finally {
+            close();
+            listener.onSocketStop(this);
+        }
+    }
+
+    public synchronized boolean sendMessage(String msg){
+        try {
+            out.writeUTF(msg);
+            out.flush();
+            return true;
+        }catch(IOException e){
+            listener.onSocketException(this, e);
+            close();
+            return false;
+        }
+    }
+
+    private synchronized void close() {
+        try {
+            out.close();
+        } catch(IOException e) {
+            listener.onSocketException(this, e);
+        }
+        interrupt();
+        try {
+            socket.close();
+        }catch(IOException e){
+            listener.onSocketException(this, e);
+        }
+    }
+}
